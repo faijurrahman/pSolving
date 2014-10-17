e2de0f41bbe28dcc2d512bf6444dfbfe8956eb63
faijurrahman <faijur@gmail.com>
2014-10-17 21:18:15 +0600

    Following structure added: - point - line - triangle - rectangle - circle - polygon

--- 2. AlgoSolution/4. geometry/basicGeometricFunction.cpp
+++ 2. AlgoSolution/4. geometry/basicGeometricFunction.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <algorithm>
 #include <iostream>
 using namespace std;
 
@@ -12,36 +13,6 @@ struct point
 	void setXY(double a, double b){x=a;y=b;}
 	double dist(point p){ return sqrt(pow(x-p.x,2)+pow(y-p.y,2));}
 	bool operator <(const point &p) const{ return x<p.x || (x==p.x && y<p.y);}
-};
-
-struct line
-{
-    public: 
-	bool ok;
-	double A,B,C; //Equation: Ax+By=C
-	//double m,mc; //Equation: y=mx+c => y=-(A/B)x + (C/B)
-	//double a,b; //Equation: x/a + y/b = 1 => x/(C/A) + y/(C/B) = 1
-
-	line(){}
-	line(double A0, double B0, double C0){A=A0;B=B0;C=C0;}
-	line(point pa, point pb){setPoint(pa,pb);}
-	void setPoint(point pa, point pb){ A=pa.y-pb.y; B=pa.x-pb.x; C=A*pa.x+B*pa.y;}
-	double dist(point pa){ return abs(A*pa.x+B*pa.y-C)/sqrt(A*A+B*B);}//Distance from a line to a point
-	line parallelLine(point pa){ return line(A,B,A*pa.x+B*pa.y);}//Parallel line goes through point pa
-	line perpendiculerLine(point pa){ return line(A,-B,A*pa.x-B*pa.y);}//Perpendiculer line goes through point pa
-	point intersect(line ab)
-	{
-		ok=false;
-		double x,y,det=A*ab.B-ab.A*B;
-		if(det)
-		{
-			x=(ab.B*1-B*ab.C)/det;
-			y=(A*ab.C-ab.A*C)/det;
-			ok=true;
-		}
-		return point(x,y);
-	}
-
 };
 
 //compute the dot product ab · bc
@@ -60,37 +31,90 @@ double crossProd(point a, point b, point c)
 	return(ab.x*ac.y - ab.y*ac.x);
 }
 
-//compute the distance from ab to c
-double linePointDist(point a, point b, point c)
+struct line
 {
-	return abs(crossProd(a,b,c)/a.dist(b));
-}
+    public: bool ok;
+	double A,B,C; //Equation: Ax+By=C
+	//double m,mc; //Equation: y=mx+c => y=-(A/B)x + (C/B)
+	//double a,b; //Equation: x/a + y/b = 1 => x/(C/A) + y/(C/B) = 1
+	line(double A0, double B0, double C0){A=A0;B=B0;C=C0;} line(){}
+	line(point pa, point pb){setPoint(pa,pb);}
+	void setPoint(point pa, point pb){ A=pa.y-pb.y; B=pa.x-pb.x; C=A*pa.x+B*pa.y;}
+	double dist(point pa){ return abs(A*pa.x+B*pa.y-C)/sqrt(A*A+B*B);}//Distance from a line to a point
+	line parallelLine(point pa){ return line(A,B,A*pa.x+B*pa.y);}//Parallel line goes through point pa
+	line perpendiculerLine(point pa){ return line(A,-B,A*pa.x-B*pa.y);}//Perpendiculer line goes through point pa
+	point intersect(line ab)
+	{
+		ok=false;
+		double x=0,y=0,det=A*ab.B-ab.A*B;
+		if(det)
+		{
+			x=(ab.B*1-B*ab.C)/det;
+			y=(A*ab.C-ab.A*C)/det;
+			ok=true;
+		}
+		return point(x,y);
+	}
+};
 
-double polygonArea(vector<point> poly)
+struct triangle
 {
-	double area = 0;
-	int X = poly.size();	
-	LOOP(i,1,X-1) area+=crossProd(poly[0],poly[i],poly[i+1]);
-	return abs(area/2.0);
-}
+    public: point A,B,C;
+	triangle(point pa, point pb, point pc){setPoint(pa,pb,pc);} triangle(){}
+	void setPoint(point pa, point pb, point pc){A=pa; B=pb; C=pc;}
+	double area(){ return crossProd(A,B,C);}
+	point side(){ return point(A.dist(B), abs(crossProd(A,B,C)/A.dist(B)));}
+};
 
-//find radious of the circle which goes though points a, b, c
-double radiousOfCircle(point a, point b, point c)
+struct rectangle
 {
-	double A1 = a.x-b.x;
-	double A2 = b.x-c.x;
-	double B1 = a.y-b.y;
-	double B2 = b.y-c.y;
-	double C1 = (A1*(a.x+b.x) + B1*(a.y+b.y))/2;
-	double C2 = (A2*(b.x+c.x) + B2*(b.y+c.y))/2;
+    public: point A,B,C;
+	rectangle(point pa, point pb, point pc){setPoint(pa,pb,pc);} rectangle(){}
+	void setPoint(point pa, point pb, point pc){A=pa; B=pb; C=pc;}
+	double area(){ return B.dist(A)*B.dist(C);}
+	double diagonal(){ return sqrt(pow(B.dist(A),2)+pow(B.dist(C),2));}
+	point side(){ return point(B.dist(A),B.dist(C));}
+};
 
-	double del = A1*B2-A2*B1;
-	if(del==0) return a.dist(point(0,0));
+struct circle
+{
+    public: point center; double radius;
+	circle(point cent, double radi){center=cent;radius=radi;} circle(){}
+	void setPoint(point cent, double radi){center=cent;radius=radi;}
+	void setPoint(point a, point b, point c)
+	{
+		double A1=a.x-b.x, A2=b.x-c.x;
+		double B1=a.y-b.y, B2=b.y-c.y;
+		double C1=(A1*(a.x+b.x)+B1*(a.y+b.y))/2;
+		double C2=(A2*(b.x+c.x)+B2*(b.y+c.y))/2;
+		double del=A1*B2-A2*B1;
+		if(del!=0){ center.y=(A1*C2-A2*C1)/del; center.x = -(B1*C2-B2*C1)/del; radius=center.dist(a);}
+	}
+	double area(){ return radius*radius*3.14159265358979;}	
+};
 
-	double y = (A1*C2-A2*C1)/del;
-	double x = -(B1*C2-B2*C1)/del;
-	return a.dist(point(x,y));
-}
+struct polygon
+{
+    public: vector<point> points;
+	polygon(vector<point> pnts){points=pnts;} polygon(){}
+	void setPoint(vector<point> pnts){points=pnts;}
+	double area()
+	{
+		double area=0;	
+		LOOP(i,1,points.size()-1) area+=crossProd(points[0],points[i],points[i+1]);
+		return abs(area/2.0);
+	}
+	vector<point> convexHull()
+	{
+		int X=points.size(), cnt=0;		
+		vector<point> hull(2*X);
+		sort(points.begin(),points.end());		
+		loop(i,X){while(cnt>=2 && crossProd(hull[cnt-2],hull[cnt-1],points[i])<=0) cnt--; hull[cnt++]=points[i];}
+		for(int i=X-2,t=cnt+1; i>=0; i--){while(cnt>=t && crossProd(hull[cnt-2],hull[cnt-1],points[i])<=0) cnt--; hull[cnt++]=points[i];}
+		hull.resize(cnt);
+		return hull;
+	}
+};
 
 //Check whether point c is inside the line segment ab
 bool onSegment(point a, point b, point c)
@@ -113,14 +137,52 @@ int main()
 	points.push_back(point(4,1.6));    //Point E
 	points.push_back(point(1.9,1));    //Point F
 
-	cout<<"Distance Between A and B = "		<< points[0].dist(points[1])					<<endl;
-	cout<<"Dot Product of points A,B,C = "	<< dotProd(points[0],points[1],points[2])		<<endl;
-	cout<<"Cross Product of points A,B,C = "<< crossProd(points[0],points[1],points[2])		<<endl;
-	cout<<"Distance of C from line AB = "	<< linePointDist(points[0],points[1],points[2])	<<endl;
-	cout<<"Area of Polygon ABCDEF = "		<< polygonArea(points)							<<endl;
+	//**********************************************************************************
+	cout<<"Point related operations:"<<endl;
+	cout<<"Distance Between A and B = "<< points[0].dist(points[1])<<endl;
+	cout<<"Dot Product of points A,B,C = "<< dotProd(points[0],points[1],points[2])<<endl;
+	cout<<"Cross Product of points A,B,C = "<< crossProd(points[0],points[1],points[2])<<endl<<endl;
+
+	//**********************************************************************************
+	cout<<"Line related operations:"<<endl;
+	line AB(points[0],points[1]);
+	cout<<"Equation of line AB "<<AB.A<<"x + "<<AB.B<<"y = "<<AB.C<<endl;
+
+	line ABparaC=AB.parallelLine(points[2]);
+	cout<<"Parallel line of AB which goes though C is "<<ABparaC.A<<"x + "<<ABparaC.B<<"y = "<<ABparaC.C<<endl;
+
+	line ABperC=AB.perpendiculerLine(points[3]);
+	cout<<"Perpendiculer line of AB which goes though D is "<<ABperC.A<<"x + "<<ABperC.B<<"y = "<<ABperC.C<<endl;
+
+	point inrsP=ABperC.intersect(ABparaC);
+	cout<<"Intersetion point of above two lines is ("<<inrsP.x<<","<<inrsP.y<<")"<<endl;
+	cout<<"Distance of E from line AB = "<< AB.dist(points[4])<<endl<<endl;
+
+	//**********************************************************************************
+	cout<<"Triangle related operations:"<<endl;
+	triangle triangl(points[0],points[1],points[2]);
+	cout<<"Area of triangle ABC = "<<triangl.area()<<endl;
+	cout<<"Base = "<<triangl.side().x<<" and height = "<<triangl.side().y<<endl<<endl;
+
+	//**********************************************************************************
+	cout<<"Rectangle related operations:"<<endl;
+	rectangle rect(points[0],points[1],points[2]);
+	cout<<"Area of triangle ABCD = "<<rect.area()<<endl;
+	cout<<"Lenght = "<<rect.side().x<<", weight = "<<rect.side().y<<" diagonal = "<<rect.diagonal()<<endl<<endl;
 	
-	cout<<endl<<"Property of the circle goes thorough points a,b,c :"<<endl;
-	cout<<"Radius = "<< radiousOfCircle(points[0],points[1],points[2])<<endl;
-	
+	//**********************************************************************************
+	cout<<"Circle related operations:"<<endl;
+	circle circ; circ.setPoint(points[0],points[1],points[2]);
+	cout<<"Property of the circle goes thorough points A,B,C :"<<endl;
+	cout<<"Center = ("<<circ.center.x<<","<<circ.center.y<<")"<<endl;
+	cout<<"Radius = "<<circ.radius<<endl;
+	cout<<"Area = "<<circ.area()<<endl<<endl;
+
+	//**********************************************************************************
+	cout<<"Polygon related operations:"<<endl;
+	polygon poly(points);
+	cout<<"Area of Polygon ABCDEF = "<< poly.area()<<endl;
+	cout<<"Number of points of covex hull obtain from ABCDEF is "<< poly.convexHull().size()<<endl<<endl;
+
     return 0;
 }
