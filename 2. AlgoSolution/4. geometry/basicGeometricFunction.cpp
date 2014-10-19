ef455850cd74ce59588d46b7153d8bc8b1066823
Faijur <faijur@gmail.com>
2014-10-19 11:01:24 +0600

    Some bug solved and some functions updated.

--- 2. AlgoSolution/4. geometry/basicGeometricFunction.cpp
+++ 2. AlgoSolution/4. geometry/basicGeometricFunction.cpp
@@ -1,60 +1,39 @@
-#include <vector>
-#include <algorithm>
-#include <iostream>
+#include<vector>
+#include<algorithm>
+#include<iostream>
+#include <sstream>
 using namespace std;
 
 #define LOOP(i,s,n) for(int i=(s);i<(n);i++)
 #define loop(i,n) for(int i=0;i<(n);i++)
+#define All(c) (c).begin(),(c).end()
 
 struct point
 {
     public: double x,y;	
 	point(double a, double b){x=a;y=b;} point(){}
-	void setXY(double a, double b){x=a;y=b;}
+	void setPoint(double a, double b){x=a;y=b;}
 	double dist(point p){ return sqrt(pow(x-p.x,2)+pow(y-p.y,2));}
+	double dot(point A, point B){point OA(A.x-x,A.y-y), OB(B.x-x,B.y-y); return(OA.x*OB.x+OA.y*OB.y);}
+	double cross(point A, point B){point OA(A.x-x,A.y-y), OB(B.x-x,B.y-y); return(OA.x*OB.y - OA.y*OB.x);}
+	bool onSegment(point A, point B){bool ok=(x<=max(A.x,B.x) && x>=min(A.x,B.x) && y<=max(A.y,B.y) && y>=min(A.y,B.y)); return ok?1:0;}
 	bool operator <(const point &p) const{ return x<p.x || (x==p.x && y<p.y);}
 };
 
-//compute the dot product ab · bc
-double dotProd(point a, point b, point c)
-{
-	point ab(b.x-a.x, b.y-a.y);
-	point bc(c.x-b.x, c.y-b.y);
-	return(ab.x*bc.x + ab.y*bc.y);
-}
-
-//compute the cross product ab x ac
-double crossProd(point a, point b, point c)
-{
-	point ab(b.x-a.x, b.y-a.y);
-	point ac(c.x-a.x, c.y-a.y);
-	return(ab.x*ac.y - ab.y*ac.x);
-}
-
+//Line equations
+//double A,B,C; //Equation: Ax+By=C
+//double m,mc; //Equation: y=mx+c => y=-(A/B)x + (C/B)
+//double a,b; //Equation: x/a + y/b = 1 => x/(C/A) + y/(C/B) = 1
 struct line
 {
-    public: bool ok;
-	double A,B,C; //Equation: Ax+By=C
-	//double m,mc; //Equation: y=mx+c => y=-(A/B)x + (C/B)
-	//double a,b; //Equation: x/a + y/b = 1 => x/(C/A) + y/(C/B) = 1
+    public: double A,B,C; bool ok;
 	line(double A0, double B0, double C0){A=A0;B=B0;C=C0;} line(){}
 	line(point pa, point pb){setPoint(pa,pb);}
 	void setPoint(point pa, point pb){ A=pa.y-pb.y; B=pa.x-pb.x; C=A*pa.x+B*pa.y;}
 	double dist(point pa){ return abs(A*pa.x+B*pa.y-C)/sqrt(A*A+B*B);}//Distance from a line to a point
 	line parallelLine(point pa){ return line(A,B,A*pa.x+B*pa.y);}//Parallel line goes through point pa
-	line perpendiculerLine(point pa){ return line(A,-B,A*pa.x-B*pa.y);}//Perpendiculer line goes through point pa
-	point intersect(line ab)
-	{
-		ok=false;
-		double x=0,y=0,det=A*ab.B-ab.A*B;
-		if(det)
-		{
-			x=(ab.B*1-B*ab.C)/det;
-			y=(A*ab.C-ab.A*C)/det;
-			ok=true;
-		}
-		return point(x,y);
-	}
+	line perpendiculerLine(point pa){ return line(-B,A,-B*pa.x+A*pa.y);}//Perpendiculer line goes through point pa
+	point intersect(line L){double D=A*L.B-L.A*B;ok=D?false:true; return D?point((L.B*C-B*L.C)/D, (A*L.C-L.A*C)/D):point(0,0);}
 };
 
 struct triangle
@@ -62,8 +41,8 @@ struct triangle
     public: point A,B,C;
 	triangle(point pa, point pb, point pc){setPoint(pa,pb,pc);} triangle(){}
 	void setPoint(point pa, point pb, point pc){A=pa; B=pb; C=pc;}
-	double area(){ return crossProd(A,B,C);}
-	point side(){ return point(A.dist(B), abs(crossProd(A,B,C)/A.dist(B)));}
+	double area(){ return abs(A.cross(B,C)/2);}
+	point side(){ return point(A.dist(B), abs(A.cross(B,C)/A.dist(B)));}
 };
 
 struct rectangle
@@ -80,6 +59,7 @@ struct circle
 {
     public: point center; double radius;
 	circle(point cent, double radi){center=cent;radius=radi;} circle(){}
+	double area(){ return radius*radius*3.14159265358979;}	
 	void setPoint(point cent, double radi){center=cent;radius=radi;}
 	void setPoint(point a, point b, point c)
 	{
@@ -90,58 +70,43 @@ struct circle
 		double del=A1*B2-A2*B1;
 		if(del!=0){ center.y=(A1*C2-A2*C1)/del; center.x = -(B1*C2-B2*C1)/del; radius=center.dist(a);}
 	}
-	double area(){ return radius*radius*3.14159265358979;}	
 };
 
 struct polygon
 {
     public: vector<point> points;
-	polygon(vector<point> pnts){points=pnts;} polygon(){}
+	polygon(vector<point> pnts){points=pnts;} polygon(){}	
 	void setPoint(vector<point> pnts){points=pnts;}
-	double area()
-	{
-		double area=0;	
-		LOOP(i,1,points.size()-1) area+=crossProd(points[0],points[i],points[i+1]);
-		return abs(area/2.0);
-	}
+	double area(){double area=0; LOOP(i,1,points.size()-1) area+=points[0].cross(points[i],points[i+1]); return abs(area/2.0);}
 	vector<point> convexHull()
 	{
-		int X=points.size(), cnt=0;		
-		vector<point> hull(2*X);
-		sort(points.begin(),points.end());		
-		loop(i,X){while(cnt>=2 && crossProd(hull[cnt-2],hull[cnt-1],points[i])<=0) cnt--; hull[cnt++]=points[i];}
-		for(int i=X-2,t=cnt+1; i>=0; i--){while(cnt>=t && crossProd(hull[cnt-2],hull[cnt-1],points[i])<=0) cnt--; hull[cnt++]=points[i];}
+		int X=points.size(), cnt=0;	vector<point> hull(2*X); sort(All(points));		
+		loop(i,X){while(cnt>=2 && hull[cnt-2].cross(hull[cnt-1],points[i])<=0) cnt--; hull[cnt++]=points[i];}
+		for(int i=X-2,t=cnt+1; i>=0; i--){while(cnt>=t && hull[cnt-2].cross(hull[cnt-1],points[i])<=0) cnt--; hull[cnt++]=points[i];}
 		hull.resize(cnt);
 		return hull;
 	}
 };
-
-//Check whether point c is inside the line segment ab
-bool onSegment(point a, point b, point c)
-{
-    if (c.x<=max(a.x,b.x) && c.x>=min(a.x,b.x) && 
-		c.y<=max(a.y,b.y) && c.y>=min(a.y,b.y))
-			return true;
-    return false;
-}
 
 // Driver program to test above functions
 int main()
 {
 
     vector<point> points;
-	points.push_back(point(0.72,2.28));//Point A
-	points.push_back(point(2.66,4.71));//Point B
-	points.push_back(point(5,3.5));    //Point C
-	points.push_back(point(3.63,2.52));//Point D
-	points.push_back(point(4,1.6));    //Point E
-	points.push_back(point(1.9,1));    //Point F
+	points.push_back(point(0,0));	//Point A
+	points.push_back(point(10,0));	//Point B
+	points.push_back(point(10,5));	//Point C
+	points.push_back(point(0,5));	//Point D
+	points.push_back(point(4,3));	//Point E
+	points.push_back(point(5,0));	//Point F
 
 	//**********************************************************************************
 	cout<<"Point related operations:"<<endl;
 	cout<<"Distance Between A and B = "<< points[0].dist(points[1])<<endl;
-	cout<<"Dot Product of points A,B,C = "<< dotProd(points[0],points[1],points[2])<<endl;
-	cout<<"Cross Product of points A,B,C = "<< crossProd(points[0],points[1],points[2])<<endl<<endl;
+	cout<<"Dot Product of points A,B,C = "<< points[0].dot(points[1],points[2])<<endl;
+	cout<<"Cross Product of points A,B,C = "<< points[0].cross(points[1],points[2])<<endl;
+	string str=points[0].onSegment(points[1],points[2])?"is ":"is not ";
+	cout<<"Point A "<<str<<"on the line segment BC"<<endl<<endl;
 
 	//**********************************************************************************
 	cout<<"Line related operations:"<<endl;
