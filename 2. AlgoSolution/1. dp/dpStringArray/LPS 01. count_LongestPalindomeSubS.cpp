13ea8cd4482a07f235486256a5d64eb053ec39b8
faijurrahman <faijur@gmail.com>
2014-11-20 14:48:30 +0600

    LPS updated

--- 2. AlgoSolution/1. dp/dpStringArray/LPS 01. count_LongestPalindomeSubS.cpp
+++ 2. AlgoSolution/1. dp/dpStringArray/LPS 01. count_LongestPalindomeSubS.cpp
@@ -1,4 +1,5 @@
 #include<string>
+#include<algorithm>
 #include<iostream>
 using namespace std;
 
@@ -7,57 +8,46 @@ using namespace std;
 #define MAX(mVal, oVal) (mVal) = max((mVal),(oVal))
 #define MIN(mVal, oVal) (mVal) = min((mVal),(oVal))
 #define ZERO(arr) memset(arr,0,sizeof(arr))
+#define FILL(arr,val) memset(arr,val,sizeof(arr))
 
-#define MOD 1000000007
-#define _N 10
-int dp[_N][_N];
+#define MAX_LEN 100
+int dp[MAX_LEN][MAX_LEN];
 
 class countLPS
 {
 public:
 	//Time = O(N2) and space = O(N2)
-	int subString(string in) {
-		int X=in.length();
-		int begin=0, maxLen=1;
-
-		ZERO(dp);loop(i,X) dp[i][i]=1;
-		LOOP(len,2,X+1) loop(i,X-len+1)
-		{
-			int j=i+len-1;
-			if(in[i]==in[j] && dp[i+1][j-1]) dp[i][j]=1, begin=i, maxLen=len;
-		}
+	int subStr(string S) 
+	{
+		int X=S.length(), begin=0, maxLen=1; ZERO(dp);
+		LOOP(len,1,X+1) for(int i=0,j=len-1; j<X; i++,j++)
+			if(S[i]==S[j] && (len<=2 || dp[i+1][j-1])) dp[i][j]=1, begin=i, maxLen=len;
 		return maxLen;
 	}
 
 	//Time = O(N2) and space = O(N2)
-	int subSequence(string in) 
+	int subSeq(string S) 
 	{
-		int X=in.length();
+		int X=S.length();
 		ZERO(dp); loop(i,X) dp[i][i]=1;
-		LOOP(len,2,X+1) loop(i,X-len+1)
-		{
-			int j=i+len-1;
-			if (in[i] == in[j]) dp[i][j]=dp[i+1][j-1]+2;
-			else dp[i][j]=max(dp[i][j-1],dp[i+1][j]);
-		}
+		LOOP(len,2,X+1) for(int i=0,j=len-1; j<X; i++,j++)
+			if(S[i]==S[j]) dp[i][j]=dp[i+1][j-1]+2;
+			else dp[i][j]=max(dp[i+1][j],dp[i][j-1]);
 		return dp[0][X-1];
 	}
-
 };
 
-// Driver program to test above function
+// Driver program to test above functions
 int main()
 {
-	countLPS lps;
-	string input;
-	cout<<"Enter Input string: ";
+	countLPS cntLPS; string input;
+	cout<<"Please enter a string: ";
 	while(cin>>input)
 	{
-		cout<<"Longest Palindomic Substring: "<<lps.subString(input)<<endl;
-		cout<<"Longest Palindomic Subsequence: "<<lps.subSequence(input)<<endl;
-		input.empty();
-		cout<<"Enter Input string: ";
+		cout<<"LCString   Len: "<<cntLPS.subStr(input)<<endl;
+		cout<<"LCSequence Len: "<<cntLPS.subSeq(input)<<endl;
+		input.empty(); cout<<endl<<"Please enter another string: ";
 	}
-
+	getchar();
 	return 0;
 }
--- 2. AlgoSolution/1. dp/dpStringArray/LPS 02. list_LongestPalindomeSubS.cpp
+++ 2. AlgoSolution/1. dp/dpStringArray/LPS 02. list_LongestPalindomeSubS.cpp
@@ -1,49 +1,71 @@
 #include<string>
+#include<algorithm>
 #include<iostream>
 using namespace std;
 
-#define MAX_LEN 200
+#define LOOP(i,s,n) for(int i=(s);i<(n);i++)
+#define loop(i,n) for(int i=0;i<(n);i++)
+#define MAX(mVal, oVal) (mVal) = max((mVal),(oVal))
+#define MIN(mVal, oVal) (mVal) = min((mVal),(oVal))
+#define ZERO(arr) memset(arr,0,sizeof(arr))
+#define FILL(arr,val) memset(arr,val,sizeof(arr))
+
+#define MAX_LEN 100
 int dp[MAX_LEN][MAX_LEN];
+
+class countLPS
+{
+public:
+	//Time = O(N2) and space = O(N2)
+	int subStr(string S) 
+	{
+		int X=S.length(), begin=0, maxLen=1; ZERO(dp);
+		LOOP(len,1,X+1) for(int i=0,j=len-1; j<X; i++,j++)
+			if(S[i]==S[j] && (len<=2 || dp[i+1][j-1])) dp[i][j]=1, begin=i, maxLen=len;
+		return maxLen;
+	}
+
+	//Time = O(N2) and space = O(N2)
+	int subSeq(string S) 
+	{
+		int X=S.length(); ZERO(dp); loop(i,X) dp[i][i]=1;
+		LOOP(len,2,X+1) for(int i=0,j=len-1; j<X; i++,j++)
+			if(S[i]==S[j]) dp[i][j]=dp[i+1][j-1]+2;
+			else dp[i][j]=max(dp[i+1][j],dp[i][j-1]);
+
+		return dp[0][X-1];
+	}
+};
 
 class listLPS
 {
 public:
 	//Time = O(N2) and space = O(N2)
-	string subString(string input)
+	string subStr(string S)
 	{
-		int n = input.length();
-		int longestBegin = 0;
-		int maxLen = 1;
-
-		//Initialize
-		memset(dp, 0, sizeof(dp));
-
-		//Base case 1
-		for (int i = 0; i < n; i++)
-			dp[i][i] = 1;
-
-
-		for (int len = 2; len <= n; len++)
-		{
-			for (int i = 0, j = len-1; j < n; i++, j++) 
-			{
-				if (input[i] == input[j] && dp[i+1][j-1]) 
-				{
-					dp[i][j] = 1;
-					longestBegin = i;
-					maxLen = len;
-				}
-			}
-		}
-		return input.substr(longestBegin, maxLen);
+		int X=S.length(), begin=0, maxLen=1; ZERO(dp);
+		LOOP(len,1,X+1) for(int i=0,j=len-1; j<X; i++,j++)
+			if(S[i]==S[j] && (len<=2 || dp[i+1][j-1])) dp[i][j]=1, begin=i, maxLen=len;
+		return S.substr(begin,maxLen);
 	}
 
 	//Time = O(N2) and space = O(N2)
-	string subSequence(string input) 
+	string subSeq(string S) 
 	{
+		int X=S.length(); ZERO(dp); loop(i,X) dp[i][i]=1;
+		LOOP(len,2,X+1) for(int i=0,j=len-1; j<X; i++,j++)
+			if(S[i]==S[j]) dp[i][j]=dp[i+1][j-1]+2;
+			else dp[i][j]=max(dp[i+1][j],dp[i][j-1]);
 
-
-		return "Not Implemented Yet";
+		string ans="",tmp=""; 	
+		for(int i=0,j=X-1; i<X && j>=0;)
+			if(dp[i][j]==0) break;	
+			else if(dp[i][j]==1){ans+=S[i]; break;}	
+			else if(dp[i][j]==dp[i][j-1]) j--;
+			else if(dp[i][j]==dp[i+1][j]) i++;
+			else ans+=S[i],tmp+=S[i],i++,j--;	
+		for(int i=tmp.size()-1; i>=0; i--) ans+=tmp[i];
+		return ans;
 	}
 
 };
@@ -51,16 +73,14 @@ public:
 // Driver program to test above function
 int main()
 {
-	listLPS lps;
-	string input="aamadamt";
-	cout<<"Enter Input string: ";
+	countLPS cntLPS; listLPS strLPS; string input;
+	cout<<"Please enter a string: ";
 	while(cin>>input)
 	{
-		cout<<"Longest Palindomic Substring: "<<lps.subString(input)<<endl;
-		cout<<"Longest Palindomic Subsequence: "<<lps.subSequence(input)<<endl;
-		input.empty();
-		cout<<"Enter Input string: ";
+		cout<<"LCString  : Len = "<<cntLPS.subStr(input)<<", Str = "<<strLPS.subStr(input)<<endl;
+		cout<<"LCSequence: Len = "<<cntLPS.subSeq(input)<<", Seq = "<<strLPS.subSeq(input)<<endl;
+		input.empty(); cout<<endl<<"Please enter another string: ";
 	}
-
+	getchar();
 	return 0;
 }
