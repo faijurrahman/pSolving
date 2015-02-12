337ed2b35eae13e49b1e01b9ae14661357d31dea
Faijur <faijur@gmail.com>
2015-02-12 22:06:14 +0600

    DFA reformatted according to my need.

--- 2. AlgoSolution/9. Pattern+DFA/1. efficientDFA.cpp
+++ 2. AlgoSolution/9. Pattern+DFA/1. efficientDFA.cpp
@@ -1,53 +1,51 @@
 //http://www.geeksforgeeks.org/pattern-searching-set-5-efficient-constructtion-of-finite-automata/
+//C Header
+#include<set>
+#include<climits>
+#include<algorithm>
+#include<vector>
 #include<string>
+#include<cstring>
 #include<iostream>
+#include <sstream>
 using namespace std;
 
+#define LOOP(i,s,n) for(int i=(s);i<(n);i++)
+#define loop(i,n) for(int i=0;i<(n);i++)
+#define MAX(mVal, oVal) (mVal) = max((mVal),(oVal))
+#define MIN(mVal, oVal) (mVal) = min((mVal),(oVal))
+#define All(c) (c).begin(),(c).end()
+#define ZERO(arr) memset(arr,0,sizeof(arr))
+#define FILL(arr,val) memset(arr,val,sizeof(arr))
+
+#define _N 100
 #define NO_OF_CHARS 256
-int (*dfa)[NO_OF_CHARS];
-void computedfa(string pattern) 
+int dfa[_N][NO_OF_CHARS];
+
+void computeDFA(string pat) 
 {
-	int pSize = pattern.size();
-	dfa = (int (*)[NO_OF_CHARS])malloc((pSize+1)*sizeof(*dfa));
-    
- 
-    // Fill entries in first row
-    for (int chars=0; chars<NO_OF_CHARS; chars++)
-       dfa[0][chars] = 0;
-
-	int lps = 0;
-    dfa[0][pattern[0]] = 1;
-    for (int state=1; state<= pSize; state++)
-    {
-        for (int chars=0; chars<NO_OF_CHARS; chars++)
-           dfa[state][chars] = dfa[lps][chars];
-
-        dfa[state][pattern[state]] = state + 1;
-        if (state < pSize) lps = dfa[lps][pattern[state]];
+	int lps=0, Y=pat.size();
+    loop(j,NO_OF_CHARS) dfa[0][j]=0; dfa[0][pat[0]]=1;
+	LOOP(i,1,Y+1)
+	{
+        loop(j,NO_OF_CHARS) dfa[i][j]=dfa[lps][j];
+        dfa[i][pat[i]]=i+1;
+		lps=dfa[lps][pat[i]];
     }
-}
- 
-void search(string text, string pattern)
-{
-    int X = text.size();
-	int Y = pattern.size();
-    computedfa(pattern);
-
-    int state = 0;
-    for (int i = 0; i < X; i++)
-    {
-       state = dfa[state][text[i]];
-	   if (state == Y) cout << "Pattern found at index " << i-Y+1 <<endl;   
-    }
-
-	free(dfa);
 }
  
 /* Driver program to test above function */
 int main()
 {
-    string txt = "GEEKS FOR GEEKS";
-    string pat = "GEEKS";
-    search(txt, pat);
+   string txt = "abababadfdsabbaabafd";
+   string pat = "aba";
+    computeDFA(pat);
+
+	int stat=0, X=txt.size(), Y=pat.size();
+	loop(i,X)
+	{
+       stat=dfa[stat][txt[i]];
+	   if(stat==Y) cout<<"Pattern found at index "<<i-Y+1<<endl;   
+    }
     return 0;
 }
